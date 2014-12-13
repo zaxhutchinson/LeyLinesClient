ae0c4b0d9a114229c81a8e9256b91d90f1a5bdac
Tamel <tamelsnash@gmail.com>
2014-12-13 14:47:02 -0500

    Gradle update

--- sliderPreference/build/generated/source/buildConfig/debug/net/jayschwa/android/preference/BuildConfig.java
+++ sliderPreference/build/generated/source/buildConfig/debug/net/jayschwa/android/preference/BuildConfig.java
@@ -5,7 +5,7 @@ package net.jayschwa.android.preference;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "net.jayschwa.android.preference";
+  public static final String APPLICATION_ID = "net.jayschwa.android.preference";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
