b68a646efc6e669a058cd9a7f777fe1728a9adfd
tsnash <tamelsnash@gmail.com>
2014-12-23 18:23:17 -0500

    Preference updates on server working

--- app/src/main/java/mycompany/myapplication/SettingsActivity.java
+++ app/src/main/java/mycompany/myapplication/SettingsActivity.java
@@ -1,13 +1,16 @@
 package mycompany.myapplication;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -27,6 +30,15 @@ import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationServices;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Map;
+
 /**
  * Created by tnash219 on 11/16/2014.
  */
@@ -34,6 +46,8 @@ public class SettingsActivity extends ActionBarActivity implements
         AdapterView.OnItemSelectedListener,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
+
+
 
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
@@ -68,6 +82,8 @@ public class SettingsActivity extends ActionBarActivity implements
         buildGoogleApiClient();
 
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+
         int lifestyleSetting, sensitivitySetting;
 
         //lifestyle init
@@ -142,8 +158,10 @@ public class SettingsActivity extends ActionBarActivity implements
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = sharedPreferences.edit();
+
         int id = adapterView.getId();
         if (id == R.id.spinner) {
             if (sharedPreferences.getBoolean("pref_key_advanced_settings",false) && sharedPreferences.getInt("pref_key_lifestyle_setting",-1) != i) {
@@ -222,14 +240,19 @@ public class SettingsActivity extends ActionBarActivity implements
                 editor.putBoolean("pref_key_advanced_settings",false);
             }
         }
-        editor.commit();
+        editor.apply();
     }
 
     public void onButtonClicked(View view) {
         int id = view.getId();
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         if (R.id.button6 == id) {
-            Toast.makeText(this, "Settings Synced", Toast.LENGTH_SHORT).show();
+            if(sharedPreferences.getBoolean("pref_key_account_setup",false)) {
+                SendPreferencesTask sendPreferencesTask = new SendPreferencesTask(this);
+                sendPreferencesTask.execute();
+            }
+            else Toast.makeText(this, "You need to set up an account in the settings menu before you can use this", Toast.LENGTH_SHORT).show();
         }
 
         if (R.id.button == id) {
@@ -291,6 +314,7 @@ public class SettingsActivity extends ActionBarActivity implements
         }
 
         @Override
+        @NonNull
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             final String uid = getArguments().getString("UID");
@@ -327,6 +351,94 @@ public class SettingsActivity extends ActionBarActivity implements
 
             // Create the AlertDialog object and return it
             return builder.create();
+        }
+    }
+
+    public class SendPreferencesTask extends AsyncTask<Void, String, String> {
+
+        Activity activity;
+        SharedPreferences sharedPreferences;
+
+        public SendPreferencesTask(Activity activity) {
+            this.activity = activity;
+        }
+
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            sharedPreferences = PreferenceManager.getDefaultSharedPreferences(activity);
+            Map<String,?> prefsMap = sharedPreferences.getAll();
+
+            //prepare all necessary settings for sync
+            StringBuilder settings = new StringBuilder();
+            for(Map.Entry<String,?> entry : prefsMap.entrySet()) {
+
+                if (!entry.getKey().equals("pref_key_UID") &&
+                        !entry.getKey().equals("pref_key_HOST") &&
+                        !entry.getKey().equals("pref_key_PORT") &&
+                        !entry.getKey().equals("pref_key_alert_status") &&
+                        !entry.getKey().equals("pref_key_tracker_enabled") &&
+                        !entry.getKey().equals("pref_key_display_status") &&
+                        !entry.getKey().equals("pref_key_automatic_path"))
+                    settings.append(entry.getKey()).append("=").append(entry.getValue().toString()).append("/n");
+            }
+
+            String status = new String();
+
+            try {
+                //prepare new socket
+                Socket socket = new Socket();
+
+                //connect to server
+                socket.connect(new InetSocketAddress(sharedPreferences.getString("pref_key_HOST", ""), Integer.parseInt(sharedPreferences.getString("pref_key_PORT", ""))), 5000);
+
+                //prepare to send message to server
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+                //text to send in order to update preferences on server
+                out.print("S\n" + //indicates command for sending preferences
+                        sharedPreferences.getString("pref_key_UID", "") + "\n" + //user id
+                        settings.toString()); //all the preferences
+
+                //done with output
+                out.flush();
+
+                //prepare to receive message from server
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                while(true) {
+                    status = in.readLine();
+                    if (status != null) break;
+                }
+
+                //stop waiting for input from server
+                out.close();
+                in.close();
+
+                //disconnect and close socket
+                socket.close();
+
+            } catch (UnknownHostException e) {
+                publishProgress("Host unreachable: " + e);
+            } catch (IOException e) {
+                publishProgress("I/O operation failed: " + e);
+            } catch(IllegalArgumentException e) {
+                publishProgress("Illegal argument: " + e);
+            }
+
+            return status;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... progress) {
+            Toast.makeText(activity, progress[0], Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onPostExecute(String status) {
+            if (!status.equals("OK")) {
+                Toast.makeText(activity, "Settings sync unsuccessful =" + status, Toast.LENGTH_LONG).show();
+            }
+            else Toast.makeText(activity, "Settings synced ", Toast.LENGTH_LONG).show();
         }
     }
 }
