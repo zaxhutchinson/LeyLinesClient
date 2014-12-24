d8506811be88b8be9a774a4fc2eaf9de38f515c7
tsnash <tamelsnash@gmail.com>
2014-12-23 22:43:02 -0500

    Fixed default settings and other minor changes

--- app/src/main/java/mycompany/myapplication/SettingsActivity.java
+++ app/src/main/java/mycompany/myapplication/SettingsActivity.java
@@ -16,6 +16,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -99,14 +100,15 @@ public class SettingsActivity extends ActionBarActivity implements
 
         lifestyleSetting = sharedPreferences.getInt("pref_key_lifestyle_setting",0);
         lifestyleTextView.setText(getResources().getStringArray(R.array.lifestyle_summaries)[lifestyleSetting]);
-
         lifestyleSpinner.setSelection(lifestyleSetting);
         lifestyleSpinner.setOnItemSelectedListener(this);
+
+
+
 
         //sensitivity init
         Spinner sensitivitySpinner = (Spinner)findViewById(R.id.spinner2);
         TextView sensitivityTextView = (TextView)findViewById(R.id.textView4);
-
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> sensitivityAdapter = ArrayAdapter.createFromResource(this,
                 R.array.sensitivity_choices, android.R.layout.simple_spinner_item);
@@ -120,27 +122,30 @@ public class SettingsActivity extends ActionBarActivity implements
         sensitivitySpinner.setSelection(sensitivitySetting);
         sensitivitySpinner.setOnItemSelectedListener(this);
 
+
         RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.test);
         CheckBox checkBox = (CheckBox)findViewById(R.id.checkBox);
         Button button = (Button)findViewById(R.id.button);
 
-        checkBox.setChecked(sharedPreferences.getBoolean("pref_key_advanced_settings",false));
 
-        if (!checkBox.isChecked()) {
+
+        if (sharedPreferences.getBoolean("pref_key_advanced_settings",false)) {
             relativeLayout.setEnabled(true);
             lifestyleSpinner.setEnabled(true);
             sensitivitySpinner.setEnabled(true);
             button.setEnabled(false);
+            checkBox.setChecked(false);
         }
         else {
             relativeLayout.setEnabled(false);
             lifestyleSpinner.setEnabled(false);
             sensitivitySpinner.setEnabled(false);
             button.setEnabled(true);
+            checkBox.setChecked(true);
         }
 
         //disallows further editing after account is created and confirmed
-        //TODO: implement disabling accounts at some point
+        //TODO: implement disabling/deleting accounts at some point
         if(sharedPreferences.getBoolean("pref_key_account_setup",false)) {
             EditText accountField = (EditText)findViewById(R.id.editTextUid);
             accountField.setText("User ID: " + sharedPreferences.getString("pref_key_UID",""));
@@ -154,6 +159,7 @@ public class SettingsActivity extends ActionBarActivity implements
             Button submit = (Button)findViewById(R.id.button5);
             submit.setEnabled(false);
         }
+
     }
 
     @Override
@@ -164,55 +170,78 @@ public class SettingsActivity extends ActionBarActivity implements
 
         int id = adapterView.getId();
         if (id == R.id.spinner) {
-            if (sharedPreferences.getBoolean("pref_key_advanced_settings",false) && sharedPreferences.getInt("pref_key_lifestyle_setting",-1) != i) {
-                Toast.makeText(this, "Lifestyle set to " + getResources().getStringArray(R.array.lifestyle_choices)[i], Toast.LENGTH_SHORT).show();
-            }
             editor.putInt("pref_key_lifestyle_setting",i);
             TextView textView = (TextView)findViewById(R.id.textView3);
             textView.setText(getResources().getStringArray(R.array.lifestyle_summaries)[i]);
+
+            //make sure these are enabled
+            editor.putBoolean("pref_key_distance_settings", true);
+            editor.putBoolean("pref_key_distance_total_settings", true);
+            editor.putBoolean("pref_key_time_settings", true);
+            editor.putBoolean("pref_key_tracker_settings", true);
+            editor.putBoolean("pref_key_tracker_disabled_setting", true);
+            editor.putBoolean("pref_key_tracker_response_setting", true);
+
             if (i == 0) {
-                editor.putBoolean("pref_key_distance_settings", Boolean.parseBoolean(getResources().getStringArray(R.array.adventurous_lifestyle)[0]));
-                editor.putString("pref_key_distance_deviation_setting",getResources().getStringArray(R.array.adventurous_lifestyle)[1]);
-                editor.putBoolean("pref_key_time_settings", Boolean.parseBoolean(getResources().getStringArray(R.array.adventurous_lifestyle)[2]));
-                editor.putString("pref_key_time_deviation_setting",getResources().getStringArray(R.array.adventurous_lifestyle)[3]);
-                editor.putBoolean("pref_key_tracker_inactive_setting", Boolean.parseBoolean(getResources().getStringArray(R.array.adventurous_lifestyle)[4]));
-                editor.putString("pref_key_tracker_inactive_duration_setting",getResources().getStringArray(R.array.adventurous_lifestyle)[5]);
+                editor.putString("pref_key_distance_deviation_relative_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[0]);
+                editor.putString("pref_key_distance_deviation_total_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[1]);
+                editor.putString("pref_key_time_deviation_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[2]);
+                editor.putString("pref_key_tracker_disabled_duration_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[3]);
+                editor.putString("pref_key_tracker_response_misses_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[4]);
             }
             else if (i == 1) {
-                editor.putBoolean("pref_key_distance_settings", Boolean.parseBoolean(getResources().getStringArray(R.array.regressive_lifestyle)[0]));
-                editor.putString("pref_key_distance_deviation_setting",getResources().getStringArray(R.array.regressive_lifestyle)[1]);
-                editor.putBoolean("pref_key_time_settings", Boolean.parseBoolean(getResources().getStringArray(R.array.regressive_lifestyle)[2]));
-                editor.putString("pref_key_time_deviation_setting",getResources().getStringArray(R.array.regressive_lifestyle)[3]);
-                editor.putBoolean("pref_key_tracker_inactive_setting", Boolean.parseBoolean(getResources().getStringArray(R.array.regressive_lifestyle)[4]));
-                editor.putString("pref_key_tracker_inactive_duration_setting",getResources().getStringArray(R.array.regressive_lifestyle)[5]);
+                editor.putString("pref_key_distance_deviation_relative_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[0]);
+                editor.putString("pref_key_distance_deviation_total_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[1]);
+                editor.putString("pref_key_time_deviation_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[2]);
+                editor.putString("pref_key_tracker_disabled_duration_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[3]);
+                editor.putString("pref_key_tracker_response_misses_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[4]);
             }
             else if (i == 2) {
-                editor.putBoolean("pref_key_distance_settings", Boolean.parseBoolean(getResources().getStringArray(R.array.child_lifestyle)[0]));
-                editor.putString("pref_key_distance_deviation_setting",getResources().getStringArray(R.array.child_lifestyle)[1]);
-                editor.putBoolean("pref_key_time_settings", Boolean.parseBoolean(getResources().getStringArray(R.array.child_lifestyle)[2]));
-                editor.putString("pref_key_time_deviation_setting",getResources().getStringArray(R.array.child_lifestyle)[3]);
-                editor.putBoolean("pref_key_tracker_inactive_setting", Boolean.parseBoolean(getResources().getStringArray(R.array.child_lifestyle)[4]));
-                editor.putString("pref_key_tracker_inactive_duration_setting",getResources().getStringArray(R.array.child_lifestyle)[5]);
+                editor.putString("pref_key_distance_deviation_relative_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[0]);
+                editor.putString("pref_key_distance_deviation_total_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[1]);
+                editor.putString("pref_key_time_deviation_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[2]);
+                editor.putString("pref_key_tracker_disabled_duration_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[3]);
+                editor.putString("pref_key_tracker_response_misses_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[4]);
             }
             else if (i == 3) {
-                editor.putBoolean("pref_key_distance_settings", Boolean.parseBoolean(getResources().getStringArray(R.array.adventurous_lifestyle)[0]));
-                editor.putString("pref_key_distance_deviation_setting",getResources().getStringArray(R.array.adventurous_lifestyle)[1]);
-                editor.putBoolean("pref_key_time_settings", Boolean.parseBoolean(getResources().getStringArray(R.array.adventurous_lifestyle)[2]));
-                editor.putString("pref_key_time_deviation_setting",getResources().getStringArray(R.array.adventurous_lifestyle)[3]);
-                editor.putBoolean("pref_key_tracker_inactive_setting", Boolean.parseBoolean(getResources().getStringArray(R.array.adventurous_lifestyle)[4]));
-                editor.putString("pref_key_tracker_inactive_duration_setting",getResources().getStringArray(R.array.adventurous_lifestyle)[5]);
+                editor.putString("pref_key_distance_deviation_relative_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[0]);
+                editor.putString("pref_key_distance_deviation_total_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[1]);
+                editor.putString("pref_key_time_deviation_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[2]);
+                editor.putString("pref_key_tracker_disabled_duration_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[3]);
+                editor.putString("pref_key_tracker_response_misses_setting", getResources().getStringArray(R.array.adventurous_lifestyle)[4]);
             }
 
         }
         else if (id == R.id.spinner2) {
-            if (sharedPreferences.getBoolean("pref_key_advanced_settings",false) && sharedPreferences.getInt("pref_key_sensitivity_setting",-1) != i) {
-                Toast.makeText(this, "Sensitivity set to " + getResources().getStringArray(R.array.sensitivity_choices)[i], Toast.LENGTH_SHORT).show();
-            }
             editor.putInt("pref_key_sensitivity_setting",i);
             TextView textView = (TextView)findViewById(R.id.textView4);
             textView.setText(getResources().getStringArray(R.array.sensitivity_summaries)[i]);
+            if (i == 0) {
+                editor.putString("pref_key_alert_frequency",getResources().getStringArray(R.array.low_sensitivity)[0]);
+                editor.putString("pref_key_gps_collect_frequency",getResources().getStringArray(R.array.low_sensitivity)[1]);
+                editor.putString("pref_key_gps_send_frequency", getResources().getStringArray(R.array.low_sensitivity)[2]);
+                editor.putFloat("pref_key_distance_relative_importance",Float.parseFloat(getResources().getStringArray(R.array.low_sensitivity)[3]));
+                editor.putFloat("pref_key_distance_total_importance",Float.parseFloat(getResources().getStringArray(R.array.low_sensitivity)[3]));
+                editor.putFloat("pref_key_time_importance",Float.parseFloat(getResources().getStringArray(R.array.low_sensitivity)[3]));
+            }
+            else if (i == 1) {
+                editor.putString("pref_key_alert_frequency",getResources().getStringArray(R.array.moderate_sensitivity)[0]);
+                editor.putString("pref_key_gps_collect_frequency",getResources().getStringArray(R.array.moderate_sensitivity)[1]);
+                editor.putString("pref_key_gps_send_frequency",getResources().getStringArray(R.array.moderate_sensitivity)[2]);
+                editor.putFloat("pref_key_distance_relative_importance",Float.parseFloat(getResources().getStringArray(R.array.moderate_sensitivity)[3]));
+                editor.putFloat("pref_key_distance_total_importance",Float.parseFloat(getResources().getStringArray(R.array.moderate_sensitivity)[3]));
+                editor.putFloat("pref_key_time_importance",Float.parseFloat(getResources().getStringArray(R.array.moderate_sensitivity)[3]));
+            }
+            else if (i == 2) {
+                editor.putString("pref_key_alert_frequency",getResources().getStringArray(R.array.high_sensitivity)[0]);
+                editor.putString("pref_key_gps_collect_frequency",getResources().getStringArray(R.array.high_sensitivity)[1]);
+                editor.putString("pref_key_gps_send_frequency",getResources().getStringArray(R.array.high_sensitivity)[2]);
+                editor.putFloat("pref_key_distance_relative_importance",Float.parseFloat(getResources().getStringArray(R.array.high_sensitivity)[3]));
+                editor.putFloat("pref_key_distance_total_importance",Float.parseFloat(getResources().getStringArray(R.array.high_sensitivity)[3]));
+                editor.putFloat("pref_key_time_importance",Float.parseFloat(getResources().getStringArray(R.array.high_sensitivity)[3]));
+            }
         }
-        editor.commit();
+        editor.apply();
     }
 
     public void onCheckboxClicked(View view) {
@@ -231,6 +260,7 @@ public class SettingsActivity extends ActionBarActivity implements
                 sensitivitySpinner.setEnabled(true);
                 button.setEnabled(false);
                 editor.putBoolean("pref_key_advanced_settings",true);
+                editor.apply();
             }
             else {
                 relativeLayout.setEnabled(false);
@@ -238,9 +268,10 @@ public class SettingsActivity extends ActionBarActivity implements
                 sensitivitySpinner.setEnabled(false);
                 button.setEnabled(true);
                 editor.putBoolean("pref_key_advanced_settings",false);
+                editor.apply();
             }
         }
-        editor.apply();
+
     }
 
     public void onButtonClicked(View view) {
@@ -413,6 +444,8 @@ public class SettingsActivity extends ActionBarActivity implements
                     if (status != null) break;
                 }
 
+                if (!status.equals("OK")) publishProgress(status);
+
                 //stop waiting for input from server
                 out.close();
                 in.close();
@@ -439,9 +472,9 @@ public class SettingsActivity extends ActionBarActivity implements
         @Override
         protected void onPostExecute(String status) {
             if (!status.equals("OK")) {
-                Toast.makeText(activity, "Settings sync unsuccessful =" + status, Toast.LENGTH_LONG).show();
+                Toast.makeText(activity, "Settings sync unsuccessful", Toast.LENGTH_LONG).show();
             }
-            else Toast.makeText(activity, "Settings synced ", Toast.LENGTH_LONG).show();
+            else Toast.makeText(activity, "Settings synced", Toast.LENGTH_LONG).show();
         }
     }
 }
